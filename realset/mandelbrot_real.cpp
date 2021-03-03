ab96d762c5ed1b8baf6bb3cddc2f5b955e5a5623
samuelelanzi <samuelelanzi00@icloud.com>
2021-03-03 09:51:13 +0100

    upload

--- realset/mandelbrot_real.cpp
+++ realset/mandelbrot_real.cpp
@@ -20,23 +20,22 @@ std::size_t points(double const& c)
 	}
 
 	return v_z.size();
-
-	v_z.clear();
 }
 
 int main() 
 {
 	std::vector<double> numbers {};
 	double c = -3.;
+	constexpr double dc = 0.01;
 
-	for (int i = 0; i != 1e3; ++i) {
+	while(c < 1.) {
 		std::size_t control = points(c);
 
 		if(control == 1001) {
 			numbers.push_back(c);
 		}
 
-		c += 0.01;
+		c += dc;
 	}
 
 	std::ofstream fout("numbers.txt");
